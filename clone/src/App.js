007591112f006ff4ae042bcd7d0ad9447af4f148
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-07-30 13:46:25 -0300

    att

--- clone/src/App.js
+++ clone/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import video from  './video.mp4'
 
 function App() {
   return (
     <div className="App">
-      
+       <div className='box-video'>
+        
+       </div>
     </div>
   );
 }
