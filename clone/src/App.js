3ae978056a2e5b33b911b1dcb5d46ebcf92029b5
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-08-01 09:02:02 -0300

    att

--- clone/src/App.js
+++ clone/src/App.js
@@ -6,15 +6,15 @@ import {FaRegCommentDots} from "react-icons/fa"
 
 function App() {
 
-  const playVideo = ()=> {
-      let video= document.getElementById('video')
+  const playVideo = ()=>{
+      let video = document.getElementById('video')
       video.play();
   }
   return (
     <div className="App">
-       <div className='box_video'>
+       <div className='box-video'>
 
-          <div className='title_video'>
+          <div className='title-video'>
             <h3>@gustavoEsposito13</h3>
             <p>My cool video</p>
 
@@ -22,16 +22,16 @@ function App() {
           <video id='video' onClick={()=>playVideo()}>
             <source src={video} type="video/mp4"/>
           </video>
-          <div className='icons_bottom'>
-              <AiOutlineUserSwitch style={{fontSize : '30px', color :'white'}}></AiOutlineUserSwitch>
-              <AiOutlineUserSwitch style={{fontSize : '30px',color :'white'}}></AiOutlineUserSwitch>
-              <AiOutlineUserSwitch style={{fontSize : '30px', color :'white'}}></AiOutlineUserSwitch>
+          <div className='icons-bottom'>
+              <AiOutlineUserSwitch style={{fontSize:'30px',color:'white'}}></AiOutlineUserSwitch>
+              <AiOutlineUserSwitch style={{fontSize:'30px',color:'white'}}></AiOutlineUserSwitch>
+              <AiOutlineUserSwitch style={{fontSize:'30px',color:'white'}}></AiOutlineUserSwitch>
 
           </div>
-          <div className='icons_right'>
+          <div className='icons-right'>
             <AiOutlineUserSwitch style={{fontSize:'30px',color: 'white',margin:'8px 0'}}></AiOutlineUserSwitch>
-            <AiFillHeart style={{fontSize:'30px',color: 'red',margin:'8px 0'}}></AiFillHeart>
-            <FaRegCommentDots style={{fontSize:'30px',color: 'white', margin:'8px 0'}}></FaRegCommentDots>
+            <AiFillHeart style={{fontSize:'30px',color:'red',margin:'8px 0'}}></AiFillHeart>
+            <FaRegCommentDots style={{fontSize:'30px',color: 'white',margin:'8px 0'}}></FaRegCommentDots>
 
           </div>
          </div> 
